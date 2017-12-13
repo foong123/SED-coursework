6b545a2c580dd6ba0cb570ddfdd06436f1701057
Foong Pak Chuen <kecy6fpc@nottingham.edu.my>
2017-12-13 08:43:37 +0800

    added "comments"

--- coursework/Vehicle_Class.h
+++ coursework/Vehicle_Class.h
@@ -1,30 +1,43 @@
+/*	This is a long header file and it contains lots of function. So the main idea of this commenting is for	
+	the user to understand how this header files works. The idea is very simple once the user understand
+	the function in base class and second level derived, then the rest of the derived class will be the same. */
 #pragma once
 #ifndef Vehicle_Class_Header
 #define Vehicle_Class_Header
 #include <iostream>
 #include <string>
 #include <iomanip>
+#define Month 12							// define month as 12 
+#define Rate 2.5							// define interest rate as 2.5
 using namespace std;
-void display();
-int coupon();
-class Vehicle
+void display();								// This is needed. It will be explained further below
+int coupon();								// This is needed because some of the functions need to call it.
+
+class Vehicle								// Base class
 {
 public:
-	string model, color;
-	Vehicle();
-	Vehicle(string, string, float);
-	float getPrice();
-	void setPrice(float);
-	void own();
-	void loan();
-	virtual void menu();
-	void chosen();
+	string model, color;					// Initializing model and color as string.
+	Vehicle();								// Default constructor
+	Vehicle(string, string, float);			// Creating a constructor
+	float getPrice();						// Getter function for price
+	void setPrice(float);					// Setter function for price
+	void chosen();							// function for chosen choice of the menu
+	void own();								// function for owning the vehicle
+	void loan();							// function for borrowing loan
+	virtual void menu();					// virtual function for menu of each class (differ than the display function)
 private:
 protected:
-	float price;
+	float price;							// hide the price 
 };
 
-Vehicle::Vehicle(string m, string c, float p)
+Vehicle::Vehicle()										// Default constructor
+{
+	model = "Default Model";
+	color = "Silver";
+	price = 0;
+}
+
+Vehicle::Vehicle(string m, string c, float p)			//overloading constructor function
 {
 	model = m;
 	color = c;
@@ -33,225 +46,241 @@ Vehicle::Vehicle(string m, string c, float p)
 	cout << "Color:" << color << endl;
 	cout << "Price:" << getPrice() << endl;
 }
-Vehicle::Vehicle()
-{
-	model = "Default Model";
-	color = "Silver";
-	price = 0;
-}
 
-float Vehicle::getPrice()
+float Vehicle::getPrice()								// getter function for price
 {
 	return price;
 }
 
-void Vehicle::setPrice(float p)
+void Vehicle::setPrice(float p)							// setter function for price
 {
 	price = p;
 }
 
-void Vehicle::own()
-{
-	cout << "Congratulation! You have own a " << model << endl;
-	cout << "The color of your car is " << color << endl;
-}
-
-void Vehicle::loan()
-{
-	char choice;
-	cout << "DO you want to borrow loan? (enter 'Y' for Yes, 'N' for No)\n";
-	cin >> choice;
-	if (choice == 'Y' || choice == 'y')
-	{
-		int year, month = 12, loan;
-		float rate = 2.5;
-		float total_interest, monthly_interest, monthly_installment;
-		cout << "Enter number of years to borrow:";
-		cin >> year;
-		cout << "Enter number of cash need to loan:";
-		cin >> loan;
-		total_interest = (rate / 100) * (float)loan * (float)year;
-		monthly_interest = total_interest / (month * year);
-		monthly_installment = ((float)loan + total_interest) / (month * year);
-		cout << "The total price need to pay is " << price - loan << endl;
-		cout << "The monthly installment is " << setprecision(2) << fixed << monthly_installment << endl;
-	}
-	else if (choice == 'N' || choice == 'n')
-	{
-		cout << "WOW! You are very rich" << endl;
-		cout << "The total price is:" << price << endl;
-	}
-	else
-	{
-		loan();
-	}
-}
-
-void Vehicle::menu()
-{
-	int q;
-	char a;
-	q = 1;
-	while (q == 1)
-	{
-		cout << "Please select one: \n\n";
-		cout << "========== General Vehicle ==========\n";
-		cout << "1. ";
-		Vehicle V1 = Vehicle("Proton Saga", "Silver", 50000);
-		cout << "\n2. ";
-		Vehicle V2 = Vehicle("Perodua Myvi", "Red", 53000);
-		cout << "\n3.";
-		Vehicle V3 = Vehicle("Proton Wira", "Silver", 45000);
-		cout << "'b' - Return to Menu\n";
-		cout << "'q' - Quit program\n" << "--------------------------\n" << ">>";
-		cin >> a;
-		cin.ignore(256, '\n');
-		if (a == '1')
-		{
-			V1.chosen();
-			V1.loan();
-			V1.own();
-			break;
-		}
-		else if (a == '2')
-		{
-			V2.chosen();
-			V2.loan();
-			V2.own();
-			break;
-		}
-		else if (a == '3')
-		{
-			V3.chosen();
-			V3.loan();
-			V3.own();
-			break;
-		}
-		else if (a == 'b' || a == 'B')
-		{
-			display();
-			break;
-		}
-		else if (a == 'q' || a == 'Q')
-		{
-			q = 2;
-			cout << "\n\nProgram quitting...\n\n";
-		}
-		else
-			cout << endl;
-	}
-}
-
-void Vehicle::chosen()
+void Vehicle::chosen()									// chosen function
 {
 	cout << "The car model you have chosen is " << model << endl;
 	cout << "The color is " << color << endl;
 }
 
-//
-class Toyota : public Vehicle
+void Vehicle::own()										// own function
+{
+	cout << "Congratulation! You have own a " << model << endl;
+	cout << "The color of your car is " << color << endl;
+}
+
+void Vehicle::loan()								// loan function
+{
+	char choice;
+	cout << "DO you want to borrow loan? (enter 'Y' for Yes, 'N' for No)\n";	// asking user to borrow loan or not	
+	cin >> choice;									// This is to read the first character of a and ignore the rest
+	cin.ignore(256, '\n');							// Ignore the rest of the character
+	if (choice == 'Y' || choice == 'y')
+	{
+		int year,loan;						// Initialize year and loan
+		float total_interest, monthly_interest, monthly_installment;		// Initialize variables
+		cout << "Enter number of years to borrow:";							// Prompt user to enter year
+		cin >> year;									
+		cout << "Enter number of cash need to loan:";						// Prompt user to enter amount of cash to loan
+		cin >> loan;
+		total_interest = (Rate / 100) * (float)loan * (float)year;			// calculation of total interest
+		monthly_installment = ((float)loan + total_interest) / (Month * year);		// calculate monthly installment
+		cout << "The total price need to pay is " << price - loan << endl;		// final payment after borrowing loan
+		cout << "The monthly installment is " << setprecision(2) << fixed << monthly_installment << endl;
+		// shows the monthly installment with 2 decimal places with setprecision and fixed. 
+	}
+	else if (choice == 'N' || choice == 'n')			
+	{
+		cout << "WOW! You are very rich" << endl;					// Telling the user rich $$$
+		cout << "The total price is:" << price << endl;				// Display original price of vehicle
+	}
+	else
+	{
+		loan();					//If user enter random characters, it will shows loan function 
+	}
+}
+
+void Vehicle::menu()				// creating the menu of a specific class. In this case it will be base class menu
+{
+	int q = 1;						// Initialize and declare q = 1 for the looping
+	char a;
+	while (q == 1)					// Display menu of vehicle
+	{
+		cout << "Please select one: \n\n";
+		cout << "========== General Vehicle ==========\n";
+		cout << "1. ";
+		Vehicle V1 = Vehicle("Proton Saga", "Silver", 50000);		// Creating object for the vehicle class 
+		cout << "\n2. ";
+		Vehicle V2 = Vehicle("Perodua Myvi", "Red", 53000);			// Creating object for the vehicle class 
+		cout << "\n3.";
+		Vehicle V3 = Vehicle("Proton Wira", "Silver", 45000);		// Creating object for the vehicle class 
+		cout << "'b' - Return to Menu\n";							
+		cout << "'q' - Quit program\n" << "--------------------------\n" << ">>";
+		cin >> a;						// This is to read the first character of a and ignore the rest
+		cin.ignore(256, '\n');			// Ignore the rest of the character
+		if (a == '1')					// Evaluating from the first character of a
+		{
+			V1.chosen();				// call the function of chosen
+			V1.loan();					// call the function of loan once chosen function is executed
+			V1.own();					// call the function of own once loan function is executed
+			break;						// break from the while loop once it is finish executing all function
+		}
+		else if (a == '2')				// similar explanation with a == '1'
+		{
+			V2.chosen();				
+			V2.loan();
+			V2.own();
+			break;
+		}
+		else if (a == '3')				// similar explanation with a == '1'
+		{
+			V3.chosen();			
+			V3.loan();
+			V3.own();
+			break;
+		}
+		else if (a == 'b' || a == 'B')	// if the user input 'b' or 'B' it returns to the main menu
+		{
+			display();					// returns to display function
+			break;
+		}
+		else if (a == 'q' || a == 'Q')	// To end the program 
+		{
+			q = 2;
+			cout << "\n\nProgram quitting...\n\n";
+		}
+		else
+			cout << endl;			//If user enter random characters,it will shows menu function 
+	}
+}
+
+// Second level of derived class
+// Toyota is the second level of derived class
+
+class Toyota : public Vehicle			//Toyota class inherit from vehicle class
 {
 public:
-	Toyota() = default;
-	Toyota(string model, string color, float price);
-	void menu() override;
-	void discount();
+	Toyota() = default;					// setting the default constructor
+	Toyota(string model, string color, float price);			// Toyota constructor
+	void menu() override;				// override menu function
+	void discount();			// discount function only available in Toyota and its derived class but not Vehicle class
 };
 
-Toyota::Toyota(string model, string color, float price) :Vehicle(model, color, price)
+Toyota::Toyota(string model, string color, float price):Vehicle(model, color, price)	// inherits constructor function from vehicle class
 {
+	// empty
 }
+
+/*	In this Toyota menu function, I won't be explaining it in detailed as it has some similarities with vehicle menu function.
+	However, I will explain some functions thats differ from the vehicle menu function.*/
 
 void Toyota::menu()
 {
 	int q;
 	char a;
-	q = 1;
 	while (q == 1)
 	{
 		cout << "Please select one: \n\n";
 		cout << "========== Special Toyota Cars ==========\n";
 		cout << "1. ";
-		Toyota T1 = Toyota("Toyota Estima", "Silver", 150000);
+		Toyota T1 = Toyota("Toyota Estima", "Silver", 150000);		// Creating object for the Toyota class 
 		cout << "\n2. ";
-		Toyota T2 = Toyota("Toyota Vellfire", "Black", 160000);
+		Toyota T2 = Toyota("Toyota Vellfire", "Black", 160000);		// Creating object for the Toyota class 
 		cout << "\n3.";
-		Toyota T3 = Toyota("Limited Edition AE 86", "Black and White", 200000);
+		Toyota T3 = Toyota("Limited Edition AE 86", "Black and White", 200000);		// Creating object for the Toyota class 
 		cout << "'b' - Return to Menu\n";
 		cout << "'q' - Quit program\n" << "--------------------------\n" << ">>";
 		cin >> a;
 		cin.ignore(256, '\n');
-		switch (a)
+		switch (a)					// Evaluating from the first character of a
 		{
 		case '1':
 			{
-				T1.chosen();
-				T1.discount();
+				T1.chosen();		// call the function of chosen
+				T1.discount();		// It can be seen that the loan and own function is missing. I will explain it in the discount function 
 			}break;
 		case '2':
 			{
-				T2.chosen();
+				T2.chosen();		// similar explanation for case '1'
 				T2.discount();
 			}break;
 		case '3':
 			{
-				T3.chosen();
+				T3.chosen();		// similar explanation for case '1'
 				T3.discount();
 			}break;
 		case 'b': case 'B':
 			{
-				display();
+				display();			// returns to main menu (display function)
 			}break;
-		case 'q': case 'Q':
+		case 'q': case 'Q':			// quit the program
 			{
-				q = 2;
+				q = 2;				
 				cout << "\n\nProgram quitting...\n\n";
 			}break;
-		default: menu();
+		default: menu();		//If user enter random characters apart from the switch case it will shows menu function 
 		}break;
 	}
 }
-void Toyota::discount()
+void Toyota::discount()		// Disount function
 {
-	char choice;
-	double disc = 0.95;
-	cout << "Do you have a coupon? (enter 'Y' for Yes, 'N' for No)\n";
+	char choice;				// initialize choice
+	double disc = 0.95;			// initialize and declare discount
+	cout << "Do you have a coupon? (enter 'Y' for Yes, 'N' for No)\n";  // asking user do they have coupon or not
 	cin >> choice;
 	cin.ignore(256, '\n');
-	if (choice == 'Y' || choice == 'y')
+	if (choice == 'Y' || choice == 'y')		// if have
 	{
-		coupon();
-		cout << "You will get a 5% discount\n";
-		price = disc * getPrice();
+		coupon();			// call this function in cpp file and execute it
+		cout << "You will get a 5% discount\n";			
+		/*	this will only happens if the user successfully guess/enter the correct coupon. Here comes the 
+			question on what happen if the user failed to guess the coupon?
+			If we have a look at the coupon function in cpp file, once the user fails to guess
+			the correct coupon the program will exit from this coupon function and leaves this line and 
+			proceed into a new main menu. The vehicle selected by the user will be deleted and the user have to 
+			select the vehicle once again	*/
+
+		price = disc * getPrice();	// calculate the new price
 		cout << "The new price of " << model << " is: " << price << endl;
-		loan();
-		own();
+		loan();				// run this once the new price is calculated
+		own();				// run this once loan is executed
 	}
-	else if (choice == 'N' || choice == 'n')
+	else if (choice == 'N' || choice == 'n')	// if dont have :(
 	{
-		cout << "No worries we still give u the discount of 5%\n";
-		price = disc * getPrice();
+		cout << "No worries we still give u the discount of 5%\n";		// discount will be still be given if dont have coupon (YEAH!)
+		price = disc * getPrice();			// calculate the new price
 		cout << "The new price of " << model << " is: " << price << endl;
-		loan();
-		own();
+		loan();		// run this once the new price is calculated
+		own();		// run this once loan is executed
 	}
 	else
 	{
-		discount();
+		discount();		//If user enter random characters, it will shows discount function 
 	}
 }
-//
-class Camry : public Toyota
+
+/* This will be the 3rd level of derived class. In the coursework, it says it needs 2 second level derived
+	class. However, remember the comments I stated earlier at the top of the header. I will explain one side of the 
+	class and the rest will the same. So I will continue with the explanation of this 3rd level derived class
+	before I partially explain the second part of the second level derived class. Camry, Altis and Vios is the 
+	3rd level of derived class. */
+
+// 3rd level class: Camry class derived from Toyota
+class Camry : public Toyota						//Camry class inherit from Toyota class and as well as vehicle class
 {
 public:
-	Camry() = default;
-	Camry(string model, string color, float price);
-	void menu() override;
+	Camry() = default;				// setting the default constructor
+	Camry(string model, string color, float price);		// Camry constructor
+	void menu() override;			// override menu function from the vehicle class
 };
 
-Camry::Camry(string model, string color, float price) :Toyota(model, color, price)
+Camry::Camry(string model, string color, float price) :Toyota(model, color, price)		// inherits constructor function from vehicle class
 {
+	//empty
 }
+
+/*	In this Camry menu function, I won't be explaining it as it has the same function and body of the 
+	Toyota menu function. Please see Toyota menu function commenting. Thank You.*/
 
 void Camry::menu()
 {
@@ -303,7 +332,8 @@ void Camry::menu()
 	}
 }
 
-//
+// 3rd level class: Altis class derived from Toyota
+// Please see Camry class and Toyota menu function commenting. Thank You.
 class Altis : public Toyota
 {
 public:
@@ -365,7 +395,9 @@ void Altis::menu()
 		}break;
 	}
 }
-//
+
+// 3rd level class: Vios class derived from Toyota
+// Please see Camry class and Toyota menu function commenting. Thank You.
 class Vios : public Toyota
 {
 public:
@@ -428,20 +460,26 @@ void Vios::menu()
 	}
 }
 
-// Honda
-class Honda : public Vehicle
+/*	This is the second part of the second level derived class. Honda will be the second level
+	derived class from the vehicle class. Once again the commenting of Honda and its 3rd level
+	derived class will not be much emphasis as it is the same as the Toyota class.*/
+
+// Honda class
+class Honda : public Vehicle		//Honda class inherit from vehicle class
 {
 public:
-	Honda() = default;
-	Honda(string model, string color, float price);
-	void menu() override;
-	void discount();
+	Honda() = default;			// setting the default constructor
+	Honda(string model, string color, float price);		 // Honda Constructor
+	void menu() override;		//overrride menu function
+	void discount();	// discount function only available in Honda and its derived class
 };
 
-Honda::Honda(string model, string color, float price) :Vehicle(model, color, price)
+Honda::Honda(string model, string color, float price) :Vehicle(model, color, price) // inherits constructor function from vehicle class
 {
+	// empty
 }
 
+// Similar commenting with Toyota menu. Please see Toyota menu function commenting
 void Honda::menu()
 {
 	int q;
@@ -492,47 +530,52 @@ void Honda::menu()
 	}
 }
 
+/*	The discount function here is similar to the Toyota. However there is a small twist in this discount 
+	function in Honda.*/
 void Honda::discount()
 {
 	char choice;
-	double disc = 0.90;
+	double disc = 0.90;			// The discount in Toyota is lesser than Honda
 	cout << "Do you have a coupon? (enter 'Y' for Yes, 'N' for No)\n";
 	cin >> choice;
 	cin.ignore(256, '\n');
 	if (choice == 'Y' || choice == 'y')
 	{
 		coupon();
-		cout << "You will get a 10% discount and a goodie bag\n" << endl;
-		cout << "The goodie bag contains shopping voucher worth RM200\n" << endl;
-		price = disc * getPrice();
+		cout << "You will get a 10% discount and a goodie bag\n" << endl;			// a free goodie bag :) 
+		cout << "The goodie bag contains shopping voucher worth RM200\n" << endl;		// more gifts !
+		price = disc * getPrice();		// Calculate the new price using Honda Discount
 		cout << "The new price of " << model << " is: " << price << endl;
-		loan();
-		own();
+		loan();		// run this once the new price is calculated	
+		own();		// run this once loan is executed
 	}
 	else if (choice == 'N' || choice == 'n')
 	{
-		cout << "Feels Bad Man\n";
-		loan();
-		own();
+		cout << "Feels Bad Man\n";		// no discount nor goodie bags if do not have coupon :(
+		loan();		// run this after 'feels bad man'
+		own();		// run this once loan is executed
 	}
 	else
 	{
 		discount();
 	}
 }
-//
-class Accord : public Honda
+
+// 3rd level class: Accord class derived from Honda
+class Accord : public Honda		//Accord class inherit from Honda class and as well as vehicle class
 {
 public:
-	Accord() = default;
-	Accord(string model, string color, float price);
-	void menu() override;
+	Accord() = default;				// setting the default constructor
+	Accord(string model, string color, float price);		// Accord constructor
+	void menu() override;			 //override menu function from the vehicle class
 };
 
-Accord::Accord(string model, string color, float price) :Honda(model, color, price)
+Accord::Accord(string model, string color, float price) :Honda(model, color, price)		// inherits constructor function from vehicle class
 {
+	//empty
 }
 
+// Please see Toyota and Honda menu function commenting. Thank You.
 void Accord::menu()
 {
 	int q;
@@ -581,7 +624,9 @@ void Accord::menu()
 		}break;
 	}
 }
-//
+
+// 3rd level class: Civic class derived from Honda
+// Please see Accord class and Honda menu function commenting. Thank You.
 class Civic : public Honda
 {
 public:
@@ -643,7 +688,9 @@ void Civic::menu()
 		}break;
 	}
 }
-//
+
+// 3rd level class: City class derived from Honda
+// Please see Accord class and Honda menu function commenting. Thank You.
 class City : public Honda
 {
 public:
--- coursework/coursework.cpp
+++ coursework/coursework.cpp
@@ -1,16 +1,26 @@
+/*	I have chosen vehicle to be my theme of this SED coursework. To be more specific, 
+	this vehicle will be more specific on car brands. I have added with some "fancies" functions so
+	that it will "beautify" the overall program. I will try my best to explain them in details as possible. Thank You. */
+
 #include <iostream>
 #include <string>
-#include <cstdlib>
-#include <time.h>
+#include <cstdlib>							
+#include <time.h>												
 #include <iomanip>
 #include "Vehicle_Class.h"
 using namespace std;
-void display();
-int coupon();
+void display();				// initialize a main menu function
+int coupon();				// initialize a coupon function
 
-void display()
+/*	Before explaining the display function(below),I would like to state that I have not follow one the instrustions 
+	in the SED Coursework instruction which says the main function must consists of arrays of pointers of 
+	base class. I chose not to do so because I need this display function so that the coupon function can use
+	this display function */
+
+//display function
+void display() 
 {
-	Vehicle arr_vehicle[100];
+	Vehicle arr_vehicle[100];			// creates an array of pointers for the base class
 	Vehicle vehicle;
 	Toyota toyota;
 	Honda honda;
@@ -20,7 +30,7 @@ void display()
 	Accord accord;
 	Civic civic;
 	City city;
-	arr_vehicle[0] = vehicle;
+	arr_vehicle[0] = vehicle;			// assigning the arrays into arrays of vehicle
 	arr_vehicle[1] = toyota;
 	arr_vehicle[2] = honda;
 	arr_vehicle[3] = camry;
@@ -29,124 +39,133 @@ void display()
 	arr_vehicle[6] = accord;
 	arr_vehicle[7] = civic;
 	arr_vehicle[8] = city;
-	Vehicle* option;
-	int i = 1;
+	Vehicle* option;					//  the array is pointed to the option
+	int i = 1;							// Initialize and declare i = 1 for the looping
 	char a;
-	while (i == 1)
+	while (i == 1)						// This will print the main menu of the program 
 	{
 		cout << "Please select one: \n\n";
-		cout << "========== MENU ==========\n" << "'1' - General Vehicles\n" << "'2' - Special Toyota Cars\n";
+		cout << "========== MAIN MENU ==========\n" << "'1' - General Vehicles\n" << "'2' - Special Toyota Cars\n";
 		cout << "'3' - Toyota Camry\n" << "'4' - Toyota Altis\n";
 		cout << "'5' - Toyota Vios\n" << "'6' - Special Honda Cars\n";
 		cout << "'7' - Honda Accors\n" << "'8' - Honda Civic\n";
 		cout << "'9' - Honda City\n";
 		cout << "'q' - Quit program\n" << "--------------------------\n" << ">>";
 		cin >> a;
-		cin.ignore(256, '\n');
-		switch (a)
+		cin.ignore(256, '\n');			// This is to read the first character of a and ignore the rest
+		switch (a)						// Evaluating from the first character of a
 		{
 		case '1':
 		{
-			option = &vehicle;
+			option = &vehicle;			/*demonstrating polymorphism by looping through the base pointer array 
+										and calling menu function*/
 			option->menu();
-		}break;
+		}break;							
 		case '2':
 		{
-			option = &toyota;
+			option = &toyota;			// similar explanation for case '1'
 			option->menu();
 		}break;
 		case '3':
 		{
-			option = &camry;
+			option = &camry;			// similar explanation for case '1'
 			option->menu();
 		}break;
 		case '4':
 		{
-			option = &altis;
+			option = &altis;			// similar explanation for case '1'
 			option->menu();
 		}break;
 		case '5':
 		{
-			option = &vios;
+			option = &vios;				// similar explanation for case '1'
 			option->menu();
 		}break;
 		case '6':
 		{
-			option = &honda;
+			option = &honda;			// similar explanation for case '1'
 			option->menu();
 		}break;
 		case '7':
 		{
-			option = &accord;
+			option = &accord;			// similar explanation for case '1'
 			option->menu();
 		}break;
 		case '8':
 		{
-			option = &civic;
+			option = &civic;			// similar explanation for case '1'
 			option->menu();
 		}break;
 		case '9':
 		{
-			option = &city;
+			option = &city;				// similar explanation for case '1'
 			option->menu();
 		}break;
-		case 'q': case 'Q':
+		case 'q': case 'Q':				// To end the program 
 		{
 			i = 2;
 			cout << "\n\nProgram quitting...\n\n";
 		}break;
-		default: display();
+		default: display();			//If user enter random characters apart from the switch case it will shows display function 
 		}break;
 	}
 }
 
+//
+/*	This coupon function is design to generate a random number so that when
+	the user enter the coupon number will be different on calling the function.
+	This will prevents cheating :) */
+
+// coupon function
 int coupon()
 {
-	srand(time(0));
+	srand(time(0));							// seeds the pseudo random generator used by the rand function
 	int number;
-	number = rand() % 10 + 1;
-	int tries = 3;
-	int guess;
+	number = rand() % 10 + 1;				//returns a random number from a range of modulo of 10. It will have a range of 1-10
+	int tries = 3;							// User is given 3 tries
+	int guess;								// User guess on the coupon
 	cout << "Coupon codes is valid between 1-10" << endl;
-	do {
-		cout << "You have " << tries << " tries remaining\n\n";
+	do {											// It will loop until tries used up or the user guessed correct
+		cout << "You have " << tries << " tries remaining\n\n";		// Stating the number of tries
 		cout << "Enter your coupon: " << endl;
 		cin >> guess;
-		tries--;
+		tries--;											// decrease in tries count
 		if (tries <= 0)
 		{
-			break;
+			break;											// the loop will break once the tries is used up
 		}
-		else if (guess < number)
+		else if (guess < number)							// check if the guess is same as the generated number
 		{
 			cout << "Invalid Coupon...\n\n" << endl;
 		}
-		else if (guess > number)
+		else if (guess > number)							// check if the guess is same as the generated number
 		{
 			cout << "Invalid Coupon...\n\n" << endl;
 		}
-	} while (guess != number);
-	if (guess == number)
+	} while (guess != number);								// End of loop once guessed correctly
+	if (guess == number)									// Check if user guessed correctly
 	{
-		cout << "Congratulations you have enter the correct coupon!\n" << endl;
+		cout << "Congratulations you have enter the correct coupon!\n" << endl;		// Display Joyfulness :) 
 	}
 	else
 	{
-		cout << "Sorry you have enter the wrong coupon\n\n";
-		display();
-		system("pause");
-		exit(0);
+		cout << "Sorry you have enter the wrong coupon\n\n";	// display unsuccessful guess :(
+		display();												// return to main menu
+		system("pause");										//	hold the console
+		exit(0);					
+		/*	Exit function from the header file and return a new display function. It is like creating a new 
+			display for the user after unsuccessful attempt getting the coupon. */
 	}
-	tries = 0;
+	tries = 0;		// declare tries = 0 after all the looping is done
 }
 
 int main()
 {
-	string name;
+	string name;										// initialize name as a string
 	cout << "Enter name please\n>> ";
-	getline(cin, name);
+	getline(cin, name);									// get the name of the user with spacing
 	cout << "\nHello Mr." << name << endl << endl;
-	display();
+	display();											// call the display function
 	system("pause");
 	return 0;
 }
